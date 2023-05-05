c74a692beff61a54cc570a6e5e8b216a9e198d25
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2023-05-06 00:36:06 +0200

    finished-conditions-structures

---
+++ 03_control_structures/01ifs.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+
+if(3 > 10){
+    echo "three is less than 10";
+} else if ( 4 > 5) {
+    echo "of course four is less than five";
+} else {
+    echo "it is not";
+
+}
+
+?>
