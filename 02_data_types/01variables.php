75cb3d29646fd84f34b41608c722102b040e8920
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2023-05-05 23:59:23 +0200

    added-basic-variable

---
+++ 02_data_types/01variables.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php 
+
+$name = "Florian";
+$num = 100;
+$numList = 100.5;
+
+echo $name . " " .$num;
+
+$name = "<h1>HELLO</h1>";
+
+echo $name;
+
+?>
